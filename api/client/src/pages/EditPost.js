bdc1f0f07a4859350335d27ed89dccea5471d7cd
SinanCayiroglu <cayiroglusinan@live.com>
2024-05-12 18:17:08 +0300

    Update EditPost.js

--- api/client/src/pages/EditPost.js
+++ api/client/src/pages/EditPost.js
@@ -12,7 +12,7 @@ export default function EditPost(){
     const [redirect,setRedirect] = useState(false)
 
     useEffect(()=>{
-        fetch("https://mern-blog-app-1-qb9j.onrender.com/post/"+id)
+        fetch("https://mern-blog-app-1-t5jg.onrender.com/post/"+id)
         .then(response=>{
             response.json().then(postInfo=>{
                 setTitle(postInfo.title)
@@ -31,7 +31,7 @@ export default function EditPost(){
         if(files?.[0]){
             data.set("file",files?.[0])
         }
-        const response = await fetch("https://mern-blog-app-1-qb9j.onrender.com/post",{
+        const response = await fetch("https://mern-blog-app-1-t5jg.onrender.com/post",{
             method:"PUT",
             body:data,
             credentials:"include"
