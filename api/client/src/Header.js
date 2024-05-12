b761ab3193a653aefb40ab302225836c4d663394
SinanCayiroglu <cayiroglusinan@live.com>
2024-05-12 17:41:42 +0300

    Update Header.js

--- api/client/src/Header.js
+++ api/client/src/Header.js
@@ -5,7 +5,7 @@ import { UserContext } from "./UserContext";
 export default function Header(){
   const {setUserInfo,userInfo} = useContext(UserContext)
   useEffect(()=>{
-    fetch("http://localhost:4000/profile",{
+    fetch("https://mern-blog-app-gsj4.vercel.app/profile",{
       credentials:"include",
     }).then(response=>{
       response.json().then(userInfo=>{
@@ -13,7 +13,7 @@ export default function Header(){
       });})
     },[])
     function logout(){
-      fetch("http://localhost:4000/logout",{
+      fetch("https://mern-blog-app-gsj4.vercel.app/logout",{
         credentials:"include",
         method:"POST",
       })
